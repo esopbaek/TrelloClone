ce9ffbc30586f61138993ec28f9f57ab7b9e9bf1
Esop Baek <esopbaek@gmail.com>
2014-09-17 06:01:01 -0400

    update production.rb for assets on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,10 +9,6 @@ TrelloClone::Application.configure do
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
-  
-  config.assets.compile = true
-
-  config.serve_static_assets = true
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
@@ -24,14 +20,14 @@ TrelloClone::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -47,7 +43,7 @@ TrelloClone::Application.configure do
   # config.force_ssl = true
 
   # Set to :debug to see everything in the log.
-  config.log_level = :info
+  config.log_level = :debug
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
